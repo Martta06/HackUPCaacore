575f53efb1855d0f0aa8ef4314d65655f1d06c3c
Marta <martablombarte@gmail.com>
2026-04-26 01:53:56 +0200

    translation

--- src/ui/role-select.js
+++ src/ui/role-select.js
@@ -2,9 +2,9 @@ export function renderRoleView() {
     return `
         <div id="pantalla-rol">
             <h1>S2B - Shop to Buy</h1>
-            <p>¿Qué quieres hacer?</p>
-            <button id="btn-rol-seller">🛍️ Vender productos</button>
-            <button id="btn-rol-buyer">🔍 Comprar productos</button>
+            <p>Choose your role</p>
+            <button id="btn-rol-seller">🛍️ Seller</button>
+            <button id="btn-rol-buyer">🔍 Buyer</button>
         </div>
     `
 }
